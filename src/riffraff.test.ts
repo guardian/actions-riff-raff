26bb84740cbc369da32dad8c23b3e8497cf25a66
Nicolas Long <nicolas.long@theguardian.com>
2022-09-07 09:54:25 +0100

    Fix build number
    core.getInput always returns a string so let's make sure we handle
    the empty string appropriately.

--- src/riffraff.test.ts
+++ src/riffraff.test.ts
@@ -1,4 +1,4 @@
-import { riffraffPrefix } from './riffraff';
+import { manifest, riffraffPrefix } from './riffraff';
 import type { Manifest } from './riffraff';
 
 describe('riffraff', () => {
@@ -16,5 +16,13 @@ describe('riffraff', () => {
 		const want = 'example/10';
 
 		expect(got).toBe(want);
+	});
+
+  it('should fallback to "dev" when buildNumber empty', () => {
+		delete process.env.GITHUB_RUN_NUMBER;
+    const got = manifest("example", undefined);
+    const want = "dev";
+
+    expect(got.buildNumber).toBe(want);
 	});
 });
